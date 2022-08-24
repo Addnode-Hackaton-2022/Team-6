e71548c2f0b13ea4f6b46d6b1332ad9c303cd6b4
Jonathan Ferm <jonteferm@users.noreply.github.com>
2022-08-24 16:45:49 +0200

    Reading fuel level

--- app/index.js
+++ app/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const WebSocket = require("ws");
 const res = require('express/lib/response');
-const app = express()
+const app = express();
 app.use(express.json());
+var fuelLevel;
 
+var calcFuelLevel = function(data) {
+    console.log("data3: " + data[3]);
+    console.log("data4: " + data[4]);
+    return (data[3] | data[4] << 8) / 50
+}
 
-
+var isSignalValue = function(data) {
+    return data[1] === 5 && data[2] === 3;
+}
 
 var connect = function() {
     const ws = new WebSocket('ws://172.16.55.42/ws')
     ws.on('open', function(){
         console.log("Socket open");
+
+        setTimeout(() => {
+            ws.send(JSON.stringify({data:[3,0,1],messagecmd:1,messagetype:17,size:3}));
+            ws.send(JSON.stringify({data:[3,0,0],messagecmd:1,messagetype:17,size:3}));
+            console.log("sent");
+        }, 1000)
     });
 
     ws.on('error', function(){
@@ -19,12 +33,18 @@ var connect = function() {
 
     ws.on('close', function(){
         console.log('socket close, reconnecting');
-        setTimeout(connect, 1000);
+        setTimeout(connect, 10);
+    });
+    
+    ws.on('message', function(msg){
+        var data = JSON.parse(msg).data;
+        
+        if(isSignalValue(data)) {
+            fuelLevel = calcFuelLevel(data);
+        } 
     });
 
-    ws.on('message', function(msg){
-        console.log(JSON.parse(msg));
-    });
+
 }
 connect();
 
@@ -32,9 +52,8 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 });
 
-app.get('/tanklevel', function() {
-    //Fråga lådan om tanklevel
-    //Return tanklevel
+app.get('/tanklevel', function(req, res) {
+    res.send(JSON.stringify({"fuelLevel": fuelLevel}));
 });
 
 app.post('/tanklevel', function(req, res){
