c5193f8631dcef540a8416ebd584e23d82b0c08e
Sandra Eliasson <sandra.eliasson@technia.com>
2022-08-24 20:26:57 +0200

    Added api endpoints/requests to backend

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,45 @@
 const express = require("express");
+const http = require("http");
+const axios = require("axios");
 const app = express();
+app.use(express.json());
 
-// db data
-const boats = ["http://localhost:3000/", "http://172.16.67.71/"];
+const vehicles = [
+  {
+    requestUrl: "http://localhost:3000",
+    name: "Vehicle1",
+    position: "59.395892, 17.958529",
+  },
+  {
+    requestUrl: "http://172.16.67.71",
+    name: "Vehicle2",
+    position: "59.395892, 17.958529",
+  },
+];
 
-// api
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello Backend");
 });
 
-app.get("/hej", (req, res) => {
-  res.send("I will be shown on the Browser");
-  console.log("I will be shown on the Terminal");
+app.get("/vehicles", (req, res) => {
+  var vehiclesRequests = vehicles.map((vehicle) =>
+    axios.get(vehicle.requestUrl.concat("/vehicledata"))
+  );
+  Promise.all(vehiclesRequests)
+    .then((res2) => {
+      console.log("statusCode: ".concat(res2.status));
+      console.log(res2.body);
+      res.send(res2.map((r) => r.body));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
-app.listen(3001);
+app.post("/addvehicle", (req, res) => {
+  vehicles = vehicles.filter((v) => v.requestUrl !== req.body.requestUrl);
+  vehicles.push(req.body);
+  res.send(req.body);
+});
+
+app.listen(3001, () => console.log("Server listening on port 3001."));
