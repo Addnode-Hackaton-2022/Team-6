8ccafdd29b014f28f98e1dac48084f404df6d813
Lundberg Rikard <SELUNDBERGRI@tetrapak.com>
2022-08-25 12:04:21 +0200

    added internalcontrolapp

---
+++ InternalControlApp/ControlApp/Controllers/MainApiController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ControlApp.Controllers
+{
+    public class MainApiController : ApiController
+    {
+        private static int Level = 50;
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        [Route("api/MainApi/LevelUp")]
+        public int LevelUp()
+        {
+            return ++Level;
+        }
+        [HttpPost]
+        [Route("api/MainApi/LevelDown")]
+        public int LevelDown()
+        {
+            return --Level;
+        }
+        [HttpGet]
+        [Route("api/MainApi/GetAlarmLevel")]
+        public int GetAlarmLevel()
+        {
+            return Level;
+        }
+        [HttpGet]
+        [Route("api/MainApi/GetTankLevel")]
+        public int GetTankLevel()
+        {
+            return 86;
+        }
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ InternalControlApp/ControlApp/Scripts/Start.js
@@ -0,0 +1,76 @@
+﻿var button1 = document.getElementById('upButton');
+button1.onclick = doUpButtonClick;
+var button2 = document.getElementById('downButton');
+button2.onclick = doDownButtonClick;
+getAlarmLevel();
+getFuelLevel();
+var alarmLevel = 0;
+
+function getFuelLevel() {
+    console.log("updating fuel");
+    $.ajax({
+        url: "http://172.16.67.71:3000/fuellevel",
+        type: "GET",
+        success: function (result) {
+            var obj = JSON.parse(result);
+            var label = document.getElementById('tankLevelLabel');
+            label.innerHTML = obj.fuelLevel;
+        }
+    });
+}
+
+function getAlarmLevel() {
+    $.ajax({
+        url: "http://172.16.67.71:3000/alarmthreshold",
+        type: "GET",
+        success: function (result) {
+            var obj = JSON.parse(result);
+            alarmLevel = obj.threshold;
+            updateAlarmLabel();
+        }
+    });
+}
+
+function setAlarmLevel() {
+    /*$.ajax({
+        url: "http://172.16.67.71:3000/alarmthreshold",
+        type: "PUT",
+        d
+        success: function (result) {
+            var label = document.getElementById('alarmLevelLabel')
+            label.innerHTML = result;
+        }
+    });*/
+
+
+    $.ajax({
+        type: 'PUT',
+        url: 'http://172.16.67.71:3000/alarmthreshold',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({"threshold": alarmLevel}) // access in body
+    }).done(function () {
+        console.log('SUCCESS');
+    }).fail(function (msg) {
+        console.log('FAIL');
+    }).always(function (msg) {
+        console.log('ALWAYS');
+    });
+}
+
+function updateAlarmLabel() {
+    var label = document.getElementById('alarmLevelLabel')
+    label.innerHTML = alarmLevel;
+}
+
+function doUpButtonClick() {
+    alarmLevel += 10;
+    updateAlarmLabel();
+    setAlarmLevel();
+}
+
+function doDownButtonClick() {
+    alarmLevel = alarmLevel - 10;
+    updateAlarmLabel();
+    setAlarmLevel();
+}
