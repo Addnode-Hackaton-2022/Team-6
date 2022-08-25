1e2d7c62da0c13717551c8dcf91e1aa584d4b559
Sandra Eliasson <sandra.eliasson@technia.com>
2022-08-25 10:19:44 +0200

    Added requests

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const http = require("http");
 const axios = require("axios");
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 let vehicles = [];
 
@@ -30,6 +37,30 @@ app.post("/addvehicle", (req, res) => {
   vehicles.push(req.body);
   console.log(req.body);
   res.send(req.body);
+});
+
+app.post("/alarmthreshold", function (req, res) {
+  const vehicle = JSON.parse(req.body);
+  axios.post(`http://${vehicle.requestUrl}/alarmthreshold`).then((res2) => {
+    res.send(vehicle.threshold);
+  });
+});
+
+app.post("/resetalarm", function (req, res) {
+  const vehicle = JSON.parse(req.body);
+  axios.post(`http://${vehicle.requestUrl}/resetalarm`).then((res2) => {
+    res.send(vehicle.resetAlarm);
+  });
+});
+
+app.post("/alarmthreshold", function (req, res) {
+  const newVehicleData = JSON.parse(req.body);
+  const vehicle = vehicles.find((vehicle) => vehicle.id === id);
+  if (vehicle) {
+    axios.post(`http://${vehicle.requestUrl}/alarmthreshold`).then((res2) => {
+      res.send(newVehicleData.threshold);
+    });
+  }
 });
 
 app.listen(3001, () => console.log("Server listening on port 3001."));
