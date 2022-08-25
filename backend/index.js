7f1f61f2dd591d4a0625f36605100ef3706f4b87
Sandra Eliasson <sandra.eliasson@technia.com>
2022-08-25 10:40:57 +0200

    Added logs

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,9 @@ app.get("/vehicles", (req, res) => {
 
   Promise.all(vehiclesRequests)
     .then((res2) => {
-      console.log(res2.map((r) => r.data));
-      res.send(res2.map((r) => r.data));
+      const allVehicles = res2.map((r) => r.data);
+      console.log("Vehicles:", allVehicles);
+      res.send(allVehicles);
     })
     .catch((error) => {
       console.error(error);
@@ -35,26 +36,33 @@ app.get("/vehicles", (req, res) => {
 app.post("/addvehicle", (req, res) => {
   vehicles = vehicles.filter((v) => v.id !== req.body.id);
   vehicles.push(req.body);
-  console.log(req.body);
+  console.log("/addvehicle", req.body);
   res.send(req.body);
 });
 
 app.post("/alarmthreshold", function (req, res) {
   const vehicle = JSON.parse(req.body);
+  console.log("/alarmthreshold", vehicle);
   axios.post(`http://${vehicle.requestUrl}/alarmthreshold`).then((res2) => {
     res.send(vehicle.threshold);
   });
 });
 
 app.post("/resetalarm", function (req, res) {
-  const vehicle = JSON.parse(req.body);
-  axios.post(`http://${vehicle.requestUrl}/resetalarm`).then((res2) => {
-    res.send(vehicle.resetAlarm);
-  });
+  const vehicleId = req.body;
+  console.log("/resetalarm vehicleId:", vehicleId);
+  const vehicle = vehicles.find((vehicle) => vehicleId === id);
+  if (vehicle) {
+    axios.post(`http://${vehicle.requestUrl}/resetalarm`).then((res2) => {
+      vehicle.resetAlarm = false;
+      res.send(false);
+    });
+  }
 });
 
 app.post("/alarmthreshold", function (req, res) {
   const newVehicleData = JSON.parse(req.body);
+  console.log("/alarmthreshold", newVehicleData);
   const vehicle = vehicles.find((vehicle) => vehicle.id === id);
   if (vehicle) {
     axios.post(`http://${vehicle.requestUrl}/alarmthreshold`).then((res2) => {
